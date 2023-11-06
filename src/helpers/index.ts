b8045465c9b82ea2f92b14f5a9714dc1ac490f7b
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2023-11-06 13:02:41 +0100

    fix: cleanup helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -19,12 +19,11 @@ export function numFormatter(num: number, digits: number) {
     { value: 1e18, symbol: "E" },
   ];
   const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-  var item = lookup
+  const item = lookup
     .slice()
     .reverse()
-    .find(function (item) {
-      return num >= item.value;
-    });
+    .find((item) => num >= item.value);
+
   return item
     ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
     : "0";
